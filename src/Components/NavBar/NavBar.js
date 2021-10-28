1365c52a23d14ca79c1689f08d6bf693a1aaa4fc
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-10-28 02:44:09 -0600

    commented out listitemtext

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -10,7 +10,7 @@ import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem'; 
 import ListItemIcon from '@mui/material/ListItemIcon'; 
-import ListItemText from '@mui/material/ListItemText';
+//import ListItemText from '@mui/material/ListItemText';
 import MailItem from '@mui/icons-material/Mail';
 
 
