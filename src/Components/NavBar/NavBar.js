51cdf63a6ede26bf870a7c890d06ac5743b5a67d
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-11-19 04:39:19 -0700

    fixed synax errors

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -5,6 +5,7 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
 import MenuIcon from '@mui/icons-material/Menu';
 import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -9,7 +9,7 @@ const Welcome = () => {
         {!identity.provisionalUser && !identity.user && (
             <h1>Welcome {identity.user?.user_metadata?.full_name}!</h1>
         )}
-      {identiy.provisionalUser && (
+      {identity.provisionalUser && (
           <h1>Thanks for signing up! Check your email to confirm.</h1>
       )}
     {identity.user && (
