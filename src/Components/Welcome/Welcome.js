b74e304ff2bcf8a187693ebc32a64ec91fd0d96d
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-11-27 21:57:00 -0700

    add styling to welcome page

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -1,11 +1,28 @@
 import { useIdentityContext } from 'react-netlify-identity-gotrue'
 
 
+
+const style = {
+    position: 'absolute', 
+    top: '50%', 
+    left: '50%', 
+    transform: 'translate(-50%, -50%)', 
+    width: 400, 
+    bgcolor: 'background.paper', 
+    border: '2px solid #000', 
+    boxShadow: 24, 
+    p: 4,
+  }
+
+
+
+
 const Welcome = () => {
     const identity = useIdentityContext()
     console.log(identity)
     return (
         <>
+         <Box sx={style}>
         {!identity.provisionalUser && !identity.user && (
             <h1>Welcome {identity.user?.user_metadata?.full_name}!</h1>
         )}
@@ -15,7 +32,7 @@ const Welcome = () => {
     {identity.user && (
         <h1>Welcome to my site {identity.user?.user_metadata?.full_name}</h1>
     )}
-
+</Box>
    </>
    )
 
