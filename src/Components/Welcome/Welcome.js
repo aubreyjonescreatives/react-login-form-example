a42de8010b9c127e73ee2751e2a4798c8662e41a
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-12-02 00:58:59 -0700

    added sign in welcome page

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -1,19 +1,7 @@
 import { Box } from '@mui/material'
 import { useIdentityContext } from 'react-netlify-identity-gotrue'
+import WelcomeSignedIn from './WelcomeSignedIn'
 
-
-
-const style = {
-    position: 'absolute', 
-    top: '50%', 
-    left: '50%', 
-    transform: 'translate(-50%, -50%)', 
-    width: 400, 
-    bgcolor: 'background.paper', 
-    border: '2px solid #000', 
-    boxShadow: 24, 
-    p: 4,
-  }
 
 
 
@@ -23,16 +11,16 @@ const Welcome = () => {
     console.log(identity)
     return (
         <>
-         <Box sx={style}>
+         <Box >
         {!identity.provisionalUser && !identity.user && (
             <h1>Welcome {identity.user?.user_metadata?.full_name}!</h1>
         )}
-      {identity.provisionalUser && (
+      {identity.provisionalUser && !identity.user && (
           
           <h1>Thanks for signing up! Check your email to confirm.</h1>
       )}
     {identity.user && (
-        <h1>Welcome to my site {identity.user?.user_metadata?.full_name}</h1>
+        <WelcomeSignedIn />
     )}
 </Box>
    </>
---
+++ src/Components/Welcome/WelcomeSignedIn.js
@@ -0,0 +1,211 @@
+import { Box } from '@mui/material'
+import { useIdentityContext } from 'react-netlify-identity-gotrue'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+
+const style = {
+   margin: '50px 200px', 
+   fontSize: '36px'
+  
+  }
+
+  const style2 = {
+    margin: '50px 200px', 
+    fontSize: '30px'
+   
+   }
+
+   const typeStyle = {
+    margin: '50px 200px', 
+    fontSize: '26px'
+   
+   }
+
+
+
+const Welcome = () => {
+    const identity = useIdentityContext()
+    console.log(identity)
+    return (
+        <>
+         <Box sx={style}>
+        <h1>Hi {identity.user?.user_metadata?.full_name}</h1>
+        </Box>
+        <Box sx={style2}>
+        <h1>What would you like to do?</h1>
+</Box>
+<Typography sx={typeStyle}>Favorited Meals</Typography>
+<Box sx={{display: 'flex', justifyContent: 'center'}}>
+<Card sx={{ maxWidth: 345, margin: '20px' }}>
+   
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+    <Card sx={{ maxWidth: 345, margin: '20px' }}>
+   
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+    <Card sx={{ maxWidth: 345, margin: '20px' }}>
+   
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+    </Box>
+
+
+
+    <Typography sx={typeStyle}>Have You Tried These?</Typography>
+<Box sx={{display: 'flex', justifyContent: 'center'}}>
+<Card sx={{ maxWidth: 345, margin: '20px' }}>
+   
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+    <Card sx={{ maxWidth: 345, margin: '20px' }}>
+   
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+    <Card sx={{ maxWidth: 345, margin: '20px' }}>
+   
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+    </Box>
+
+
+
+
+    <Typography sx={typeStyle}>Account Details</Typography>
+<Box sx={{display: 'flex', justifyContent: 'center'}}>
+<Card sx={{ maxWidth: 345, margin: '20px' }}>
+   
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Account Settings
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Steps to take to recover account username, password, etc.
+        </Typography>
+      </CardContent>
+    </Card>
+    <Card sx={{ maxWidth: 345, margin: '20px' }}>
+   
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Account Privacy
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Learn more about account privacy and the extreme measures we take to make sure 
+          your information is safe. 
+        </Typography>
+      </CardContent>
+    </Card>
+    <Card sx={{ maxWidth: 345, margin: '20px' }}>
+   
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Account Payment History
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          We thank you so much for being a Dine n Dash account user! Here hosts every 
+          payment detail made to Dine n Dash.  
+        </Typography>
+      </CardContent>
+
+    </Card>
+    </Box>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+   </>
+   )
+
+}
+
+export default Welcome
