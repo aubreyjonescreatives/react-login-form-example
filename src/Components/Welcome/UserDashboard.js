380197925459759aaada0a6aee7e595f84cb127b
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-12-12 08:49:58 -0700

    ui updates

--- src/Components/Welcome/UserDashboard.js
+++ src/Components/Welcome/UserDashboard.js
@@ -82,58 +82,7 @@ console.log(favorites)
 
 
 
-    <Typography sx={typeStyle}>Have You Tried These?</Typography>
-<Box sx={{display: 'flex', justifyContent: 'center'}}>
-<Card sx={{ maxWidth: 345, margin: '20px' }}>
-   
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-    <Card sx={{ maxWidth: 345, margin: '20px' }}>
-   
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-    <Card sx={{ maxWidth: 345, margin: '20px' }}>
-   
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-    </Box>
-
+  
 
 
 
